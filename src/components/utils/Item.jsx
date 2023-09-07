f3887d05bc260db4c40c685e9408856395d6578b
AKGENERAL <atharavakumbhar18@gmail.com>
2023-09-07 05:43:48 +0530

    Second

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -1,44 +1,35 @@
-import { StarIcon,ShoppingBagIcon } from '@heroicons/react/24/solid'
+import { StarIcon,ShoppingBagIcon} from '@heroicons/react/24/solid'
 import React from 'react'
 
-const Item = ({
-  ifExists,
-  id,
-  color,
-  shadow,
-  title,
-  text,
-  img,
-  btn,
-  rating,
-  price,
-}
-
-) => {
+const Item = ({id,color,shadow,title,text,img,btn,rating,price}) => {
+  console.log(id)
   return (
-    <div className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center ${
-      ifExists ? "justify-items-start" : "justify-items-center"
-    } rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}>
-      <div className={`grid items-center ${
-            ifExists ? "justify-items-start" : "justify-items-center"
-          }`}>
-        <h1 className=''>{title}</h1>
-        <p className=''>{text}</p>
-        <div className=''>
-          <div className=''><h1>{price}</h1></div>
-          <div className=''><StarIcon className='icon-style text-slate-900'/><h1>{rating}</h1></div>
-          <h1></h1>
+    <>
+    <div 
+     className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}>
+      <div className='grid items-center justify-items-center'>
+        <h1 className='text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow'>{title}</h1>
+        <p className='text-slate-200 filter drop-shadow text-base md:text-sm font-normal'>{text}</p>
+        <div className='flex items-center bg-white/80 px-1 rounded'>
+          <div className='text-black text-sm font-medium'>${price}</div>
+          <div className="flex items-center gap-1"><StarIcon className='icon-style w-5 h-5 md:w-4 md:h-4'/><h1 className='md:text-sm font-normal text-slate-100'>{rating}</h1></div>
         </div>
-        <div className=''>
-          <button type='button' className=''><ShoppingBagIcon className='icon-style text-slate-900'/></button>
-          <button type='button' className=''>{btn}</button>
+        <div className='flex items-center gap-3'>
+          <button type='button' className='bg-white/90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200'><ShoppingBagIcon className="icon-style text-slate-900"/></button>
+          <button type='button' className='bg-white/90 blur-effect-theme button-theme px-2 py-1 shadow shadow-sky-200 text-sm text-black'>{btn}</button>
         </div>
       </div>
-      <div className=''>
-        <img src={img} alt="img/item-img" className=''/>
+      <div>
+        <img
+          src={img}
+          alt="img/item-img"
+          className='h-36 w-64 transitions-theme hover:-rotate-12'
+        />
       </div>
     </div>
+    </>
   )
 }
 
 export default Item
+
