b69b3e4344f6d6bcd42cec4808cc08891dcb0ac7
Atharva <atharvakumbhar18@gmail.com>
2023-10-19 17:18:27 +0530

    half

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Hero,Sales } from './components';
-import { heroapi,popularsales,toprateslaes} from './data/data.js';
+import { FlexContent, Hero,Sales, Stories } from './components';
+import { heroapi,popularsales,toprateslaes,highlight,sneaker, story} from './data/data.js';
 
 const App = () => {
   return (
     <div>
     <main className='flex flex-col gap-16 relative'>
       <Hero heroapi={heroapi}/>
-      <Sales endpoint={popularsales}/>
+      <Sales endpoint={popularsales} ifExists/>
+      <FlexContent endpoint={highlight}/>
       <Sales endpoint={toprateslaes}/>
+      <FlexContent endpoint={sneaker}/>
+      <Stories story={story}/>
     </main>
     </div>
   )
