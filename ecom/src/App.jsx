e17c1b047b11cd1a60b8a246cc3ed16339b2e5cb
Atharva <atharvakumbhar18@gmail.com>
2023-10-31 12:03:37 +0530

    Final

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Hero } from './components';
-import { heroapi } from './data/data.js';
+import {Cart, FlexContent, Footer, Hero,Navbar,Sales, Stories } from './components';
+import { heroapi,popularsales,toprateslaes,highlight,sneaker, story, footerAPI} from './data/data.js';
 
 const App = () => {
   return (
     <div>
-    <main>
+      <Navbar/>
+      <Cart/>
+    <main className='flex flex-col gap-16 relative'>
       <Hero heroapi={heroapi}/>
+      <Sales endpoint={popularsales} ifExists/>
+      <FlexContent endpoint={highlight}/>
+      <Sales endpoint={toprateslaes}/>
+      <FlexContent endpoint={sneaker}/>
+      <Stories story={story} />
     </main>
+    <Footer footerAPI={footerAPI}/>
     </div>
   )
 }
